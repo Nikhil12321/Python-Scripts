883384fbbde3226d3b8cf114cef62faddb2d0dd5
Nikhil12321 <bhatia.nikhil95@gmail.com>
2017-10-02 11:27:29 +0530

    nikhil

--- git.sh
+++ git.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # read commit name and push to master all changes
 
+echo "commit name:"
+read commit_name
 git add *
-git commit -m "regular"
+git commit -m $commit_name
 
 git push -u origin master
