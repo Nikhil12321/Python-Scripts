7c99bf055176b733f9387808698f4b380281b674
Nikhil12321 <bhatia.nikhil95@gmail.com>
2017-10-02 11:06:32 +0530

    tria

--- git.sh
+++ git.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+
 # read commit name and push to master all changes
 
 read commitname
 git add *
 git commit -m $commitname
-git push -u origin master
+
+#!/bin/expect
+spawn git push -u origin master
+expect "*sername*"
+send -- "nikhil12321"
+
+expect "*assword*"
+send -- "1234pass"
