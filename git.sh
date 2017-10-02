9feaad72dd45ab12fe12c546cf4527fa12941010
Nikhil12321 <bhatia.nikhil95@gmail.com>
2017-10-02 11:08:09 +0530

    nikhil

--- git.sh
+++ git.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
+#!/bin/expect
 # read commit name and push to master all changes
 
 read commitname
 git add *
 git commit -m $commitname
 
-#!/bin/expect
 spawn git push -u origin master
 expect "*sername*"
 send -- "nikhil12321"
